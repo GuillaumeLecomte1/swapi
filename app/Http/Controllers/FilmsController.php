4b5b3c3c188948973a7287638c1e20e8679a398c
Guillaume Lecomte <guillaume.lecomte049@outlook.fr>
2024-03-05 11:11:24 +0100

    la dernière pour le Davy

--- app/Http/Controllers/FilmsController.php
+++ app/Http/Controllers/FilmsController.php
@@ -57,7 +57,7 @@ class FilmsController extends Controller
  *     summary="Afficher un film à l'aide de son id",
  *     tags={"Films"},
  *     @OA\Response(response=400, description="Invalid request"),
- *     @OA\Response(response="200", description="Retourne la liste des films")
+ *     @OA\Response(response="200", description="Retourne le film ciblé par l'id passé en paramètre")
  * )
  */
     public function read(string $id)
@@ -93,105 +93,14 @@ class FilmsController extends Controller
  *     path="/api/films",
  *     summary="Créé un film",
  *     tags={"Films"},
- *     @OA\RequestBody(
- *         required=true,
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(property="title", type="string", example="A New Hope"),
- *                 @OA\Property(property="episode_id", type="integer", example=4),
- *                 @OA\Property(property="opening_crawl", type="string", example="It is a period of civil war..."),
- *                 @OA\Property(property="director", type="string", example="George Lucas"),
- *                 @OA\Property(property="producer", type="string", example="Gary Kurtz, Rick McCallum"),
- *                 @OA\Property(property="release_date", type="string", format="date", example="1977-05-25"),
- *                 @OA\Property(
- *                     property="planets",
- *                     type="array",
- *                     @OA\Items(type="string", format="uri"),
- *                     example={1}
- *                 ),
- *                 @OA\Property(
- *                     property="characters",
- *                     type="array",
- *                     @OA\Items(type="string", format="uri"),
- *                     example={1}
- *                 ),
- *                 @OA\Property(
- *                     property="starships",
- *                     type="array",
- *                     @OA\Items(type="string", format="uri"),
- *                     example={2}
- *                 ),
- *                 @OA\Property(
- *                     property="vehicles",
- *                     type="array",
- *                     @OA\Items(type="string", format="uri"),
- *                     example={4}
- *                 ),
- *                 @OA\Property(
- *                     property="species",
- *                     type="array",
- *                     @OA\Items(type="string", format="uri"),
- *                     example={1}
- *                 ),
- *             )
- *         )
- *     ),
  *     @OA\Response(response=400, description="Invalid request"),
  *     @OA\Response(response="200", description="Création d'un film")
  * )
  */
-public function create(Request $request)
-{
-    $request->validate([
-        'title' => 'required|string|max:255',
-        'episode_id' => 'required|integer',
-        'opening_crawl' => 'required|string',
-        'director' => 'required|string|max:255',
-        'producer' => 'required|string|max:255',
-        'release_date' => 'required|date',
-        'planets' => 'array',      
-        'characters' => 'array',   
-        'starships' => 'array',    
-        'vehicles' => 'array',     
-        'species' => 'array',      
-    ]);
-
-    $film = Films::create([
-        'title' => $request->input('title'),
-        'episode_id' => $request->input('episode_id'),
-        'opening_crawl' => $request->input('opening_crawl'),
-        'director' => $request->input('director'),
-        'producer' => $request->input('producer'),
-        'release_date' => $request->input('release_date'),
-    ]);
-
-    if ($request->has('planets')) {
-        $film->planets()->attach($request->input('planets'));
+    public function create(Request $request)
+    {
+        $films = Films::create($request->all());
     }
-
-    if ($request->has('characters')) {
-        $film->characters()->attach($request->input('characters'));
-    }
-
-    if ($request->has('starships')) {
-        $film->starships()->attach($request->input('starships'));
-    }
-
-    if ($request->has('vehicles')) {
-        $film->vehicles()->attach($request->input('vehicles'));
-    }
-
-    if ($request->has('species')) {
-        $film->species()->attach($request->input('species'));
-    }
-
-    return response()->json([
-        'message' => 'Film created successfully',
-        'film' => $film,
-    ], 201);   
-}
-
 
 /**
  * @OA\Put(
@@ -205,107 +114,16 @@ public function create(Request $request)
  *       type="integer"
  *       )
  *   ),
- *     @OA\RequestBody(
- *         required=true,
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(property="title", type="string", example="A New Hope"),
- *                 @OA\Property(property="episode_id", type="integer", example=4),
- *                 @OA\Property(property="opening_crawl", type="string", example="It is a period of civil war..."),
- *                 @OA\Property(property="director", type="string", example="George Lucas"),
- *                 @OA\Property(property="producer", type="string", example="Gary Kurtz, Rick McCallum"),
- *                 @OA\Property(property="release_date", type="string", format="date", example="1977-05-25"),
- *                 @OA\Property(
- *                     property="planets",
- *                     type="array",
- *                     @OA\Items(type="string", format="text"),
- *                     example={1}
- *                 ),
- *                 @OA\Property(
- *                     property="characters",
- *                     type="array",
- *                     @OA\Items(type="string", format="text"),
- *                     example={1}
- *                 ),
- *                 @OA\Property(
- *                     property="starships",
- *                     type="array",
- *                     @OA\Items(type="string", format="text"),
- *                     example={2}
- *                 ),
- *                 @OA\Property(
- *                     property="vehicles",
- *                     type="array",
- *                     @OA\Items(type="string", format="text"),
- *                     example={4}
- *                 ),
- *                 @OA\Property(
- *                     property="species",
- *                     type="array",
- *                     @OA\Items(type="string", format="text"),
- *                     example={1}
- *                 ),
- *             )
- *         )
- *     ),
  *     summary="Modifier un film",
  *     tags={"Films"},
  *     @OA\Response(response=400, description="Invalid request"),
- *     @OA\Response(response="200", description="Retourne la liste des films")
+ *     @OA\Response(response="200", description="Modification d'un film")
  * )
  */
     public function update(Request $request, string $id)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'episode_id' => 'required|integer',
-            'opening_crawl' => 'required|string',
-            'director' => 'required|string|max:255',
-            'producer' => 'required|string|max:255',
-            'release_date' => 'required|date',
-            'planets' => 'array',      
-            'characters' => 'array',   
-            'starships' => 'array',    
-            'vehicles' => 'array',     
-            'species' => 'array',      
-        ]);
-    
-        $film = Films::findOrFail($id);
-    
-        $film->update([
-            'title' => $request->input('title'),
-            'episode_id' => $request->input('episode_id'),
-            'opening_crawl' => $request->input('opening_crawl'),
-            'director' => $request->input('director'),
-            'producer' => $request->input('producer'),
-            'release_date' => $request->input('release_date'),
-        ]);
-    
-        if ($request->has('planets')) {
-            $film->planets()->sync($request->input('planets'));
-        }
-    
-        if ($request->has('characters')) {
-            $film->characters()->sync($request->input('characters'));
-        }
-    
-        if ($request->has('starships')) {
-            $film->starships()->sync($request->input('starships'));
-        }
-    
-        if ($request->has('vehicles')) {
-            $film->vehicles()->sync($request->input('vehicles'));
-        }
-    
-        if ($request->has('species')) {
-            $film->species()->sync($request->input('species'));
-        }
-    
-        return response()->json([
-            'message' => 'Film mis à jour',
-            'film' => $film,
-        ], 200);
+        $films = Films::find($id);
+        $films->update($request->all());
     }
 
 /**
@@ -323,7 +141,7 @@ public function create(Request $request)
  *     summary="Supprimer un film",
  *     tags={"Films"},
  *     @OA\Response(response=400, description="Invalid request"),
- *     @OA\Response(response="200", description="Retourne la liste des films")
+ *     @OA\Response(response="200", description="Suppression d'un film")
  * )
  */
     public function destroy(string $id)
--- routes/api.php
+++ routes/api.php
@@ -25,55 +25,60 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('status.euh')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 // User routes
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/register', [LoginController::class, 'register'])->name('register');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('test/test', [LoginController::class, 'testtest']);
 
-Route::middleware([AuthJwt::class])->group(function(){
 
-// People routes
-Route::get('/people', [PeopleController::class, 'readAll']);
-Route::get('/people/{id}', [PeopleController::class, 'read']);
-Route::post('/people', [PeopleController::class, 'create']);
-Route::put('/people/{id}', [PeopleController::class, 'update']);
-Route::delete('/people/{id}', [PeopleController::class, 'destroy']);
+Route::middleware([AuthJwt::class])->middleware('status.euh')->group(function(){
 
-// Planets routes
-Route::get('/planets', [PlanetsController::class, 'readAll']);
-Route::get('/planets/{id}', [PlanetsController::class, 'read']);
-Route::post('/planets', [PlanetsController::class, 'create']);
-Route::put('/planets/{id}', [PlanetsController::class, 'update']);
-Route::delete('/planets/{id}', [PlanetsController::class, 'destroy']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Films routes
-Route::get('/films', [FilmsController::class, 'readAll']);
-Route::get('/films/{id}', [FilmsController::class, 'read']);
-Route::post('/films', [FilmsController::class, 'create']);
-Route::put('/films/{id}', [FilmsController::class, 'update']);
-Route::delete('/films/{id}', [FilmsController::class, 'destroy']);
+    // People routes
+    Route::get('/people', [PeopleController::class, 'readAll']);
+    Route::get('/people/{id}', [PeopleController::class, 'read']);
+    Route::post('/people', [PeopleController::class, 'create']);
+    Route::put('/people/{id}', [PeopleController::class, 'update']);
+    Route::delete('/people/{id}', [PeopleController::class, 'destroy']);
 
-// Species routes
-Route::get('/species', [SpeciesController::class, 'readAll']);
-Route::get('/species/{id}', [SpeciesController::class, 'read']);
-Route::post('/species', [SpeciesController::class, 'create']);
-Route::put('/species/{id}', [SpeciesController::class, 'update']);
-Route::delete('/species/{id}', [SpeciesController::class, 'destroy']);
+    // Planets routes
+    Route::get('/planets', [PlanetsController::class, 'readAll']);
+    Route::get('/planets/{id}', [PlanetsController::class, 'read']);
+    Route::post('/planets', [PlanetsController::class, 'create']);
+    Route::put('/planets/{id}', [PlanetsController::class, 'update']);
+    Route::delete('/planets/{id}', [PlanetsController::class, 'destroy']);
 
-// Vehicles routes
-Route::get('/vehicles', [VehiclesController::class, 'readAll']);
-Route::get('/vehicles/{id}', [VehiclesController::class, 'read']);
-Route::post('/vehicles', [VehiclesController::class, 'create']);
-Route::put('/vehicles/{id}', [VehiclesController::class, 'update']);
-Route::delete('/vehicles/{id}', [VehiclesController::class, 'destroy']);
+    // Films routes
+    Route::get('/films', [FilmsController::class, 'readAll']);
+    Route::get('/films/{id}', [FilmsController::class, 'read']);
+    Route::post('/films', [FilmsController::class, 'create']);
+    Route::put('/films/{id}', [FilmsController::class, 'update']);
+    Route::delete('/films/{id}', [FilmsController::class, 'destroy']);
 
-// Starships routes
-Route::get('/starships', [StarshipsController::class, 'readAll']);
-Route::get('/starships/{id}', [StarshipsController::class, 'read']);
-Route::post('/starships', [StarshipsController::class, 'create']);
-Route::put('/starships/{id}', [StarshipsController::class, 'update']);
-Route::delete('/starships/{id}', [StarshipsController::class, 'destroy']);
+    // Species routes
+    Route::get('/species', [SpeciesController::class, 'readAll']);
+    Route::get('/species/{id}', [SpeciesController::class, 'read']);
+    Route::post('/species', [SpeciesController::class, 'create']);
+    Route::put('/species/{id}', [SpeciesController::class, 'update']);
+    Route::delete('/species/{id}', [SpeciesController::class, 'destroy']);
+
+    // Vehicles routes
+    Route::get('/vehicles', [VehiclesController::class, 'readAll']);
+    Route::get('/vehicles/{id}', [VehiclesController::class, 'read']);
+    Route::post('/vehicles', [VehiclesController::class, 'create']);
+    Route::put('/vehicles/{id}', [VehiclesController::class, 'update']);
+    Route::delete('/vehicles/{id}', [VehiclesController::class, 'destroy']);
+
+    // Starships routes
+    Route::get('/starships', [StarshipsController::class, 'readAll']);
+    Route::get('/starships/{id}', [StarshipsController::class, 'read']);
+    Route::post('/starships', [StarshipsController::class, 'create']);
+    Route::put('/starships/{id}', [StarshipsController::class, 'update']);
+    Route::delete('/starships/{id}', [StarshipsController::class, 'destroy']);
 
 
 });
